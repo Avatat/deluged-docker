a124ed7058bc31748ab2169cb8e0ec9426e26056
Bartosz Zieba <bartosz@zieba.pro>
2022-01-16 20:28:24 +0100

    Fix getting Deluge version

--- check-build-push.sh
+++ check-build-push.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 git clone -b develop --single-branch git://deluge-torrent.org/deluge.git
+cd deluge
 
-version=$(python3 deluge/version.py)
+version=$(python3 version.py)
 latest_tag_count=$(curl --silent https://hub.docker.com/v2/repositories/avatat/deluged/tags/?name="$version" | jq '.count')
 
 if [ "$latest_tag_count" -gt 0 ]
@@ -10,6 +11,8 @@ then
     echo "Latest commit build is currently on Docker Hub. Exiting."
     exit 0
 fi
+
+cd ..
 
 docker login -u avatat -p "$HUB_PASSWORD"
 docker build -t avatat/deluged:"$version" .
